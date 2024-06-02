aaeb4915f200d1dd251bfd726bf94d1eea0c3429
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-06-03 00:09:49 +0600

    cupon and footer section added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,80 @@
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+// import footerphoto from "../assets/images/footer.jpg";
+const footerphoto =true
+
+const Footer = () => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `linear-gradient(180deg,rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(${footerphoto})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+        className="bg-gray-600 text-white"
+      >
+        <div className="container mx-auto p-4 ">
+          <div className="md:flex justify-between flex-wrap gap-5 pb-16 border-b-2 border-gray-400 border-opacity-70">
+            <div className="mt-8 ">
+              <h1 className="text-4xl font-bold font-play">Vanguard Estates</h1>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-2xl font-semibold">Address</h1>
+              <h6 className="font-semibold opacity-80">
+                Germany-785 15h Street.
+              </h6>
+              <h6 className="font-semibold opacity-80">Office 478</h6>
+              <h6 className="font-semibold opacity-80">Berlin.Be 81566</h6>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-2xl font-semibold">Contact Us</h1>
+              <h6 className="font-semibold opacity-80">info@email.com</h6>
+              <h6 className="font-semibold opacity-80">+18400000000</h6>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-2xl font-semibold">Useful Links</h1>
+              <Link to="/">
+                <h6 className="font-semibold opacity-80 hover:opacity-100 hover:underline">
+                  Home
+                </h6>
+              </Link>
+              <Link to="/allcarft">
+                <h6 className="font-semibold opacity-80 hover:opacity-100 hover:underline">
+                  All Foods
+                </h6>
+              </Link>
+              <Link to="/addcarft">
+                <h6 className="font-semibold opacity-80 hover:opacity-100 hover:underline">
+                  Gallery
+                </h6>
+              </Link>
+            </div>
+          </div>
+          <div className=" md:flex justify-between text-center mt-12 pb-6">
+            <div>
+              <p>©2024 Flavor Junction all right reserved.</p>
+            </div>
+            <div className="flex gap-3 lg:gap-6 text-center justify-center mt-3 md:mt-0">
+              <div className="lg:text-3xl text-xl hover:cursor-pointer">
+                <FaFacebook />
+              </div>
+              <div className="lg:text-3xl text-xl hover:cursor-pointer">
+                <FaTwitter />
+              </div>
+              <div className="lg:text-3xl text-xl hover:cursor-pointer">
+                <FaInstagram />
+              </div>
+              <div className="lg:text-3xl text-xl hover:cursor-pointer">
+                <FaGithub />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,15 +3,74 @@ import SectionTitle from "../Components/SectionTitle";
 import Slider from "../Components/Slider";
 
 const Home = () => {
-    return (
-        <div className="text-center">
-            <Slider></Slider>
-            <div className="mt-10">
-            <SectionTitle title={"hello"} para={"hello bangladesh"}></SectionTitle>
-            <AboutBuilding></AboutBuilding>
+  return (
+    <div className="text-center">
+      <Slider></Slider>
+      <div className="mt-10">
+        <SectionTitle title={"hello"} para={"hello bangladesh"}></SectionTitle>
+        <AboutBuilding></AboutBuilding>
+      </div>
+
+      {/* coupons */}
+      <div className="container mx-auto mb-32">
+        <SectionTitle
+          title={"coupons"}
+          para={
+            "Enthusiastically synthesize scalable niches via clicks-and-mortar deliverables. Monotonectally exploit emerging expertise via empowered deliverables. Monotonectally."
+          }
+        ></SectionTitle>
+        <div className=" grid lg:grid-cols-2 gap-6">
+          {/* coupons cards */}
+          <div className=" bg-gradient-to-tl from-violet-500 to-fuchsia-500 p-8 rounded-xl  relative overflow-hidden">
+            <h1 className="text-white">Coupon!!!</h1>
+            <p className="text-white">
+              Enthusiastically synthesize scalable niches via clicks-and-mortar
+              deliverables. Monotonectally exploit emerging expertise via
+              empowered deliverables. Monotonectally.
+            </p>
+            <div className="join">
+              <input
+              type="text"
+              readOnly
+              defaultValue={"nsjoiwe"}
+                className="input input-bordered join-item focus:outline-none"
+                placeholder=""
+              />
+              <button className="btn join-item ">
+                Subscribe
+              </button>
             </div>
+              <div className=" border p-8 rounded-full w-20 h-20 bg-white absolute top-14 -right-12"></div>
+              <div className=" border p-8 rounded-full w-20 h-20 bg-white absolute top-14 -left-12"></div>
+          </div>
+          <div className=" bg-gradient-to-br from-sky-500 to-indigo-500 p-8 rounded-xl  relative overflow-hidden">
+            <h1 className="text-white">Coupon!!!</h1>
+            <p className="text-white">
+              Enthusiastically synthesize scalable niches via clicks-and-mortar
+              deliverables. Monotonectally exploit emerging expertise via
+              empowered deliverables. Monotonectally.
+            </p>
+            <div className="join">
+              <input
+              type="text"
+              readOnly
+              defaultValue={"nsjoiwe"}
+                className="input input-bordered join-item"
+                placeholder=""
+              />
+              <button className="btn join-item ">
+                Subscribe
+              </button>
+            </div>
+              <div className=" border p-8 rounded-full w-20 h-20 bg-white absolute top-14 -right-12"></div>
+              <div className=" border p-8 rounded-full w-20 h-20 bg-white absolute top-14 -left-12"></div>
+          </div>
+          <div className="border bg-red-500">duck</div>
+          <div className="border bg-red-500">duck</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
